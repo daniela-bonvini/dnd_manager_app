0e01537b10868ebb6b2812138bd8a635b2021705
Daniela Bonvini <danbonvini@gmail.com>
2025-11-12 20:06:12 +0100

    created base app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dnd_manager_app
+
 An app to manage d&amp;d inventory and spells
+
+# To start
+
+cd dnd-manager-app
+npm install
+npm run dev
---
+++ dnd-manager-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Hello D&D World!</div>
+    </>
+  );
+}
+
+export default App;
