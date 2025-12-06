3968136fcdcea16d4e6e1185691649ddea66c23e
Daniela Bonvini <danbonvini@gmail.com>
2025-12-06 21:27:14 +0100

    added vitest and react testing libraries

---
+++ dnd-manager-app/src/test/setup.ts
@@ -0,0 +1,30 @@
+import "@testing-library/jest-dom";
+
+// Mock localStorage for tests
+const localStorageMock = (() => {
+  let store: Record<string, string> = {};
+
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+    key: (index: number) => {
+      const keys = Object.keys(store);
+      return keys[index] || null;
+    },
+    get length() {
+      return Object.keys(store).length;
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", {
+  value: localStorageMock,
+});
